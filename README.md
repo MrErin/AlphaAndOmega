47a7a25cfd7aa5a27c0824ef9eb70b72afb83009
MrErin <erinmeaker@gmail.com>
2018-04-18 19:50:52 -0500

    finished colorizers

--- README.md
+++ README.md
@@ -6,3 +6,11 @@
 Completed Features:
 * Create new cards, including classes for styling
 * Delete button that removes the associated card
+* Background colorizer
+* Font colorizer
+
+Remaining features:
+* Styles
+
+Stretch Goals:
+* Color randomizer
--- cardBuilder.js
+++ cardBuilder.js
@@ -19,7 +19,22 @@ const newCard = () => {
 	deleteButton.setAttribute ('id','button' + idNum)
 	deleteButton.onclick = function() {deleteCard(card.id)}
 	card.appendChild(deleteButton)
-    
+	
+	// create bgcolor button
+	const bgColorPicker = document.createElement('input')
+	bgColorPicker.setAttribute('type', 'color')
+	bgColorPicker.setAttribute('id', 'bgPicker' + idNum)
+	bgColorPicker.onchange = function() {bgColor(card.id, this.value)}
+	card.appendChild(bgColorPicker)
+
+	//create font color button
+	const fontColorPicker = document.createElement('input')
+	fontColorPicker.setAttribute('type', 'color')
+	fontColorPicker.setAttribute('id', 'fontPicker' + idNum)
+	fontColorPicker.onchange = function() {fontColor(card.id, this.value)}
+	card.appendChild(fontColorPicker)
+
+
 	//create the text and add it to the card
 	const cardText = document.createElement('p')
 	cardText.classList = 'userText'
@@ -29,9 +44,4 @@ const newCard = () => {
 	//add the card to the DOM
 	contentHook.appendChild(card)
 	idNum++
-}
-
-const deleteCard = (cardNum) => {
-	const cardToRemove = document.getElementById(cardNum)
-	contentHook.removeChild(cardToRemove)
 }
