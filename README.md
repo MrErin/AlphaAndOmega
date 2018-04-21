e6820a8ce5eccf9ff76c4de4223afe5d9dd457cc
MrErin <erinmeaker@gmail.com>
2018-04-21 15:44:44 -0500

    finished styles

--- README.md
+++ README.md
@@ -1,21 +1,26 @@
 # Alpha and Omega
 
-
 [Exercise requirements](https://github.com/nashville-software-school/the-vault/blob/master/ALPHAOMEGA.md)
 
 Completed Features:
+
 * Create new cards, including classes for styling
 * Delete button that removes the associated card
 * Background colorizer
 * Font colorizer
 * Random colorizer
+* update DOM builder for correctly styling cards
+* Styles
+  * create button: add btn class
+  * create separation between button area and text area in card
+  * textarea: centered, add font and background styling
+  * put delete button at bottom of card or at top right
+  * group two color pickers together, with randomizer underneath
+  * add text to explain the color pickers
 
 Remaining features:
-* Styles
-    * create separation between button area and text area in card
-    * group two color pickers together, with randomizer underneath
-    * put delete button at bottom of card or at top right
-    * textarea: centered, add font and background styling
-    * create button: add btn class
+
 
 Stretch Goals:
+
+* wrap text if very long text is entered (after research this might require jQuery. Not sure I want to tackle that at the moment.)
--- scripts/cardBuilder.js
+++ scripts/cardBuilder.js
@@ -4,48 +4,82 @@ const contentHook = document.querySelector('#cardBox')
 //create iterator for card IDs
 let idNum = 0
 
+//div factory
+const div = (classes) => {
+	let div = document.createElement('div')
+	div.classList = classes
+	return div
+}
+
 //when the user enters text into the text area and then clicks create, create a new card element in the DOM.
 
 const newCard = () => {
 	//create the card and add a class
 	const card = document.createElement('div')
-	card.classList = 'card'
+	card.classList = 'card wrapper column'
 	card.setAttribute('id', 'card' + idNum)
+
+	//create div for delete button
+	const delBtnDiv = div()
+	card.appendChild(delBtnDiv)
     
 	//create the delete button
 	const deleteButton = document.createElement('button')
-	deleteButton.classList = 'btn'
-	deleteButton.textContent = 'Delete Card'
+	deleteButton.classList = 'btn-delete'
+	deleteButton.textContent = 'X'
 	deleteButton.onclick = function() {deleteCard(card.id)}
-	card.appendChild(deleteButton)
+	delBtnDiv.appendChild(deleteButton)
+
+	//card's bordered outer div
+	const outerDiv = div('wrapper border column')
+	card.appendChild(outerDiv)
+	
+	//div for colorizers
+	const colorDiv = div('wrapper')
+	outerDiv.appendChild(colorDiv)
+	
+	//p for bg colorizer
+	const bgText = document.createElement('p')
+	bgText.textContent = 'Select Card Background Color: '
+	colorDiv.appendChild(bgText)
 	
 	// create bgcolor button
 	const bgColorPicker = document.createElement('input')
 	bgColorPicker.classList = 'picker'
 	bgColorPicker.setAttribute('type', 'color')
 	bgColorPicker.onchange = function() {bgColor(card.id, this.value)}
-	card.appendChild(bgColorPicker)
+	bgText.appendChild(bgColorPicker)
+
+	//p for font colorizer
+	const fontText = document.createElement('p')
+	fontText.textContent = 'Select Font Color: '
+	colorDiv.appendChild(fontText)
 
 	//create font color button
 	const fontColorPicker = document.createElement('input')
-	bgColorPicker.classList = 'picker'
+	fontColorPicker.classList = 'picker'
 	fontColorPicker.setAttribute('type', 'color')
 	fontColorPicker.onchange = function() {fontColor(card.id, this.value)}
-	card.appendChild(fontColorPicker)
+	fontText.appendChild(fontColorPicker)
 	
 	//create a randomizer button
 	const randomizerButton = document.createElement('button')
 	randomizerButton.classList = 'btn'
 	randomizerButton.textContent = 'Randomize!'
 	randomizerButton.onclick = function() {randomizeCard(card.id)}
-	card.appendChild(randomizerButton)
+	outerDiv.appendChild(randomizerButton)
+
+
+	//create div for the user text
+	const userText = div('wrapper')
+	card.appendChild(userText)
 
 
 	//create the text and add it to the card
 	const cardText = document.createElement('p')
 	cardText.classList = 'userText'
 	cardText.textContent = document.getElementById('textForCard').value
-	card.appendChild(cardText)
+	userText.appendChild(cardText)
     
 	//add the card to the DOM
 	contentHook.appendChild(card)
